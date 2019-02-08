6b233531f1eeb09af0676cc0db184c7da94893d7
epogrebnyak <e.pogrebnyak@gmail.com>
2019-02-08 11:51:25 +0300

    haskell refactoring

--- accounting.hs
+++ accounting.hs
@@ -1,71 +1,111 @@
 -- Minimal double-entry accounting system
 
-import Data.Map (fromList, alter)
 
-data Type = Asset | Expense | Equity | Liability | Income | Profit deriving Show
-data Account = Account Type [Float] [Float] deriving Show
+import qualified Data.Map.Internal
+import Data.Map (fromList, toList, alter, lookup)
 
--- функции для увеличения либо правой либо левой стороны счета 
+type Amount = Float
+data Category = Asset | Expense | Equity | Liability | Income | Profit deriving Show
+type Name = String
+data Account = Account Category [Amount] [Amount] deriving Show
+data Side = Debit | Credit
+data Ref = Ref Name deriving Show
+
+type AccountMap = Data.Map.Internal.Map Name Account
+type OperationMap = Data.Map.Internal.Map Name (Ref, Ref)
+
+nullAccount :: Category -> Account  
+nullAccount t = Account t [] []
+
+category :: Account -> Category
+category (Account cat debits credits) = cat
+
+sumSide Asset = Debit
+sumSide Expense = Debit
+sumSide _ = Credit
+
+
+balance :: Account -> Amount
+balance (Account cat debits credits) = case sumSide cat of
+    Debit -> s
+    Credit -> -s 
+    where s = sum(debits) - sum(credits) 
+
+-- функции для увеличения либо правой, либо левой стороны счета 
+append :: [a] -> a -> [a]
 append xs x = xs ++ [x]
+debit :: Amount -> Account -> Account
 debit x (Account t debits credits) = Account t (append debits x) credits 
+credit :: Amount -> Account -> Account
 credit x (Account t debits credits) = Account t debits (append credits x)
 
--- Нам нужно сгруппировать конструктору из Type в две группы, так чтобы 
--- функция balance в одной группы выдавала выражение (debits - credits),
--- а в другой группе (credits-debits).
+-- план счетов 
+fromListWith :: (a -> (String, b)) -> [a] -> Data.Map.Internal.Map String b
+fromListWith f x = fromList (map f x)
 
--- Сейчас это достигается системой проверок is_debit. Можно ли 
--- свернуть в более элегантную реализацию? Идея из других языков:
--- наследовать "типы"  
--- Account -> DebitAccount | CrediAccount
--- DebitAccount -> Asset | Expense
--- CrediAccount -> Equity | Liability | Income | Profit
--- и далее balance могла бы вести себя по-разному в зависимости 
--- от типа DebitAccount или DebitAccount
-is_debit Asset = True
-is_debit Expense = True
-is_debit _ = False
-balance (Account t debits credits) = s * (if is_debit t then 1 else -1)   
-    where s = sum(debits) - sum(credits)  
+makeAccountMap :: [(Category, String)] -> AccountMap
+makeAccountMap = fromListWith f 
+    where f (t, name) = (name::Name,  nullAccount t) 
 
--- Возможно мне нужно что-то из конструкций ниже 
--- но тут мне не хватает ума понять, как будут введены конкретные 
--- типы Asset | Expense | Equity | Liability | Income | Profit
-type Amount = Float
-class TAccount a where
-    debit :: Amount -> a -> a   
-    credit :: Amount -> a -> a 
-    balance :: a -> Amount
-instance TAccount DebitAccount where
-    -- здесь balance делает что-то одно 
-    balance :: a -> Amount
-instance TAccount CreditAccount where
-    -- здесь balance делает что-то другое 
-    balance :: a -> Amount
-
-
--- Далее в целом работет, текущих вопросов нет.
-makeAccounts accNames = fromList $ map makeAccount accountNames  
-    where makeAccount (t, name) = (name, Account t [] [])
-debit' x (Just acc) = Just (debit x acc)  
-credit' x (Just acc) = Just (credit x acc)
-
+-- makeOperationsMap :: [(String, String, String)] -> AccountMap
+makeOperationsMap :: [(String, String, String)] -> OperationMap
+makeOperationsMap = fromListWith f
+    where f (a, b, c)  = (a::Name, (Ref b, Ref c)) 
+    
+-- проводка     
 data Entry = Entry {
-    debitAccount :: String, 
-    creditAccount :: String, 
-    amount :: Float
-    } 
+    debitAccount :: Ref, 
+    creditAccount :: Ref, 
+    amount :: Amount
+    }
 
-processEntry accounts (Entry da ca x) = alter (credit' x) ca $ alter (debit' x) da accounts
+process :: AccountMap -> Entry -> AccountMap
+mutate accounts (Entry (Ref debitAccount) (Ref creditAccount) amount) = 
+    -- alter f key m, where f is function, and m is map 
+    alter credit' creditAccount $ alter debit' debitAccount accounts
+    where 
+        debit' (Just account) = Just (debit amount account)  
+        debit' Nothing = Nothing 
+        credit' (Just account) = Just (credit amount account)
+        credit' Nothing = Nothing
+
+getEntry :: OperationMap -> Name -> Amount -> Entry 
+getEntry operationMap op amount = case Data.Map.lookup op operationMap of
+        Just (r1, r2) -> Entry r1 r2 amount
+        Nothing -> error "Wrong operation"   
+
+makeOperation :: OperationMap -> AccountMap -> Name -> Amount -> AccountMap
+makeOperation operationMap accountMap op amount = mutate accountMap e1
+    where e = getEntry operationMap op amount
+
+-- операции
+
+allowedEntries = [
+    ("borrow", "cash", "debt"),
+    ("accrue interest", "interest", "debt"),
+    ("pay debt", "debt", "cash")
+    ]
 
 accountNames = [ 
     (Asset, "cash"),
-    (Equity, "capital")
+    (Equity, "capital"),
+    (Expense, "interest"),
+    (Liability, "debt")
     ]
 
--- Пробуем тут     
+
+-- Примеры    
 a = Account Asset [] []
 z = balance $ credit 7 (debit 10 a) -- 3
-accounts = makeAccounts accountNames 
-e = Entry "cash" "capital" 5
-accounts' = processEntry accounts e
+accounts = makeAccountMap accountNames 
+e1 = Entry (Ref "cash") (Ref "capital") 5
+accounts' = mutate accounts e1
+e2 = Entry (Ref "cash") (Ref "debt") 2
+accounts'' = mutate accounts' e2
+ops = makeOperationsMap allowedEntries
+process = makeOperation ops 
+accounts4 = makeOperation ops accounts'' "pay debt" 2
+
+-- Далее
+-- укоротить импорты 
+-- Name -> AccountName OperationName
--- lm.jl
+++ lm.jl
@@ -1,77 +1,106 @@
 # https://docs.julialang.org/en/v1/manual/types/index.html
-mutable struct TAccount
+mutable struct DebitAccount
     debits :: Vector # fo REadl
-    credit :: Vector # fo REadl
+    credits :: Vector # fo REadl
 end
 
-struct LinearModel
-    observed::Sample
-    intercept::Bool
-    beta::Vector #of Real
+mutable struct CreditAccount
+    debits :: Vector # fo REadl
+    credits :: Vector # fo REadl
+end
+
+
+# function debit(acc :: TAccount)
+#     return sum(acc.debits) -  sum(acc.credits)
+# end
+    
+# function credit(acc :: TAccount)
+#     return -debit(acc)
+# end
+
+function debit_sum(acc)
+    return sum(acc.debits) - sum(acc.credits)
+end
+
+function balance(acc :: DebitAccount)
+    return debit_sum(acc)
 end    
 
-function has_intercept(lm::LinearModel)
-  return all(lm.observed[:,1] .== 1.0) 
+function balance(acc :: CreditAccount)
+    return -debit_sum(acc)
 end  
+    
 
-# OLS estimation using (\)
-# https://github.com/giob1994/Alistair.jl/blob/3a11c19150169695581b46e4d1895f0641a4c29d/src/linregress.jl#L38-L41
-function ols(X::Array, Y::Array)::Array
-    return (X' * X) \ (X' * Y)
-end 
 
-"""Fit ordinary linear regression for observations."""
-function ols(sample::Sample; intercept::Bool=false)::LinearModel
-    f = intercept ? add_intercept : identity
-    beta_hat = ols(f(sample.X), sample.Y)
-    return LinearModel(sample, intercept, beta_hat)
-end
 
-"""Return fitted dependent variable Y."""
-function yhat(lm::LinearModel)
-    f = lm.intercept ? add_intercept : identity
-    return f(lm.observed.X) * lm.beta 
-end    
+# struct LinearModel
+#     observed::Sample
+#     intercept::Bool
+#     beta::Vector #of Real
+# end    
 
-function equation(lm::LinearModel, precision::Int)  
-    return equation(lm.beta, lm.intercept, precision)  
-end    
+# function has_intercept(lm::LinearModel)
+#   return all(lm.observed[:,1] .== 1.0) 
+# end  
 
-function equation(beta::Vector, intercept::Bool, precision::Int=4)
-    beta = map(x -> round(x, digits=precision), beta)
-    result = "Y = "
-    if intercept
-        result *= "$(beta[1])"
-        beta = beta[2:end]
-    end
-    for (i,b) in enumerate(beta)
-        result *= (b >= 0 ? " + $b" : " - $(abs(b))") * "*X$i"
-    end    
-    return result
-end    
+# # OLS estimation using (\)
+# # https://github.com/giob1994/Alistair.jl/blob/3a11c19150169695581b46e4d1895f0641a4c29d/src/linregress.jl#L38-L41
+# function ols(X::Array, Y::Array)::Array
+#     return (X' * X) \ (X' * Y)
+# end 
 
-function desc(lm::LinearModel)::String
-    quack_ = lm.intercept ? " " : " no "
-    precision = 4
-    r2_ = round(r2(lm), digits=precision)
-    eq_ = equation(lm, precision)
-    join_(args...) = join(args,"\n")
-    return join_("Linear model with$(quack_)intercept: $eq_",                 
-                 "Coefficients: $(lm.beta)",
-                 "R-squared: $(r2_)")
-end    
+# """Fit ordinary linear regression for observations."""
+# function ols(sample::Sample; intercept::Bool=false)::LinearModel
+#     f = intercept ? add_intercept : identity
+#     beta_hat = ols(f(sample.X), sample.Y)
+#     return LinearModel(sample, intercept, beta_hat)
+# end
 
-show(lm::LinearModel) = println(desc(lm))
+# """Return fitted dependent variable Y."""
+# function yhat(lm::LinearModel)
+#     f = lm.intercept ? add_intercept : identity
+#     return f(lm.observed.X) * lm.beta 
+# end    
 
-sum_of_squares(x::Vector)::Real = sum(x .^ 2) 
+# function equation(lm::LinearModel, precision::Int)  
+#     return equation(lm.beta, lm.intercept, precision)  
+# end    
 
-"""Residual sum of squares."""
-rss(lm::LinearModel) = sum_of_squares(yhat(lm) - lm.observed.Y)
+# function equation(beta::Vector, intercept::Bool, precision::Int=4)
+#     beta = map(x -> round(x, digits=precision), beta)
+#     result = "Y = "
+#     if intercept
+#         result *= "$(beta[1])"
+#         beta = beta[2:end]
+#     end
+#     for (i,b) in enumerate(beta)
+#         result *= (b >= 0 ? " + $b" : " - $(abs(b))") * "*X$i"
+#     end    
+#     return result
+# end    
 
-"""Total sum of squares for Y - mean.
-   Also equals var(Y)*n.
-"""
-tss(lm::LinearModel) = sum_of_squares(lm.observed.Y .- mean(lm.observed.Y)) # 
+# function desc(lm::LinearModel)::String
+#     quack_ = lm.intercept ? " " : " no "
+#     precision = 4
+#     r2_ = round(r2(lm), digits=precision)
+#     eq_ = equation(lm, precision)
+#     join_(args...) = join(args,"\n")
+#     return join_("Linear model with$(quack_)intercept: $eq_",                 
+#                  "Coefficients: $(lm.beta)",
+#                  "R-squared: $(r2_)")
+# end    
 
-"""R2 (unadjusted) = 1-(RSS/TSS)""" 
-r2(lm::LinearModel) = 1 - rss(lm)/tss(lm)
+# show(lm::LinearModel) = println(desc(lm))
+
+# sum_of_squares(x::Vector)::Real = sum(x .^ 2) 
+
+# """Residual sum of squares."""
+# rss(lm::LinearModel) = sum_of_squares(yhat(lm) - lm.observed.Y)
+
+# """Total sum of squares for Y - mean.
+#    Also equals var(Y)*n.
+# """
+# tss(lm::LinearModel) = sum_of_squares(lm.observed.Y .- mean(lm.observed.Y)) # 
+
+# """R2 (unadjusted) = 1-(RSS/TSS)""" 
+# r2(lm::LinearModel) = 1 - rss(lm)/tss(lm)
